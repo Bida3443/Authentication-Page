5d9504ecc5308c1f30939c73af46eebcbdc5f82e
Bida3443 <alexanderbamise@gmail.com>
2025-08-07 13:41:50 -0700

    Created the add section of the todo list

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,103 +1,89 @@
+"use client";
+
 import Image from "next/image";
+import { useState } from "react";
+import { createClient } from "@/utils/supabase/client";
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  type Tasks = {
+    tasks: string;
+  };
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  const [open, setOpen] = useState(false);
+  const [tasks, setTasks] = useState<Tasks>({
+    tasks: "",
+  });
+
+  const handleInput = (e: any) => {
+    const { name, value } = e.target;
+    setTasks({ ...tasks, [name]: value });
+  };
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+
+    try {
+      const supabase = await createClient();
+
+      const { data, error } = await supabase
+        .from("todo")
+        .insert([{ duties: tasks }])
+        .select();
+
+      if(data) {
+        alert("Inserted sucessfully")
+      }
+
+    } catch (error) {
+      console.error(error)
+      alert(error)
+    }
+  };
+
+  return (
+    <section className="bg-slate-900 flex justify-center items-center h-[100dvh]">
+      <div className="bg-amber-50 flex flex-col h-[80dvh] w-160">
+        <div className="bg-amber-50 flex justify-between items-center h-[80dvh] w-160">
+          <div className="text-black ml-7 font-bold text-[50px]">TODO APP</div>
+
+          <button
+            onClick={() => setOpen(true)}
+            className="bg-slate-900 flex p-5 rounded-full mr-5 w-7 h-7 font-bold text-[30px] items-center justify-center hover:cursor-pointer"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            +
+          </button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+        <div className="text-black flex justify-start bg-gray-300 p-5 m-8 mt-1 h-[180dvh] font-bold text-[20px] pl-2">
+          Add Task
+        </div>
+      </div>
+
+      {open && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center">
+          <div
+            onClick={() => setOpen(false)}
+            className="absolute w-full h-[100dvh] bg-black opacity-80"
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          <div className="flex gap-8 p-5 bg-white w-100 flex-col relative">
+            <form onSubmit={handleSubmit} className="flex flex-col gap-8 text-black font-bold size-20px">
+              <label className="text-black font-bold text-2xl">Task Name</label>
+              <input
+                type="text"
+                name="tasks"
+                id="tasks"
+                onChange={handleInput}
+                className="border-black border-1 text-black p-2 w-full"
+              />
+              <button
+                type="submit"
+                className="flex justify-center items-center bg-amber-950 border-1 text-white border-amber-950 p-3 hover:cursor-pointer hover:bg-red-500 "
+              >
+                Add
+              </button>
+            </form>
+          </div>
+        </div>
+      )}
+    </section>
   );
 }
